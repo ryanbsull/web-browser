4f319920f5ced500f783378995cb001bdddf053e
Ryan B. Sullivan <ryanbakersullivan@gmail.com>
2025-05-16 10:01:48 -0400

    browser: accept command line args
    Signed-off-by: Ryan B. Sullivan <ryanbakersullivan@gmail.com>

--- browser.c
+++ browser.c
@@ -1,1 +1,12 @@
-int main() { return 0; }
+#include <stdio.h>
+#include <stdlib.h>
+
+int error(const char* msg) {
+  perror(msg);
+  exit(1);
+}
+
+int main(int argc, char* argv[]) {
+  if (argc != 2) error("ERROR: incorrect number of args");
+  return 0;
+}
