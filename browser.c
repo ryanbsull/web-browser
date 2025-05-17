97d4503edebcd5c2a54462fb5e1c80e617e86f24
Ryan B. Sullivan <ryanbakersullivan@gmail.com>
2025-05-17 19:39:28 -0400

    web-browser: add in libraries for later HTTP requests
    Signed-off-by: Ryan B. Sullivan <ryanbakersullivan@gmail.com>

--- browser.c
+++ browser.c
@@ -1,5 +1,10 @@
+#include <netdb.h>
+#include <netinet/in.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <unistd.h>
 
 int error(const char* msg) {
   perror(msg);
