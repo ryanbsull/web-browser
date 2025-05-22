50649b21e41aaa835b0b3cc010716ad707eef011
Ryan B. Sullivan <ryanbakersullivan@gmail.com>
2025-05-22 14:08:54 -0400

    web-browser: current status of http_request function
    TODO: determine where error:
    - `nodename nor servname provided, or not known`
    is coming from?
    
    Signed-off-by: Ryan B. Sullivan <ryanbakersullivan@gmail.com>

--- browser.c
+++ browser.c
@@ -7,38 +7,53 @@
 #include <sys/socket.h>
 #include <unistd.h>
 
+#define BUFFER_SIZE 4096
+
 int error(const char* msg) {
   perror(msg);
   exit(1);
 }
 
 int http_request(const char* addr) {
-  int port = 80, in = 1;
+  printf("GET: %s\n", addr);
+  int port = 80, status;
   const char* req = "GET /\r\n";
+  char resp[BUFFER_SIZE];
 
-  struct hostent* server;
-  struct sockaddr_in serv_addr;
-  int sockfd, bytes, sent, received, total;
-  char msg[1024], resp[4096];
+  struct addrinfo hints = {0}, *addrs;
+  hints.ai_family = AF_UNSPEC;
+  hints.ai_socktype = SOCK_STREAM;
+  hints.ai_protocol = IPPROTO_TCP;
 
-  if ((server = gethostbyname(addr)) == NULL) error("gethostbyname");
+  if ((status = getaddrinfo(addr, "80", &hints, &addrs)) != 0) {
+    char err_msg[1024];
+    sprintf(err_msg, "%s: %s", addr, gai_strerror(status));
+    error(err_msg);
+  }
+  int sockfd;
 
-  memcpy(&serv_addr.sin_addr, &server->h_addr, server->h_length);
-  serv_addr.sin_port = htons(port);
-  serv_addr.sin_family = AF_INET;
-  sockfd = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
-  setsockopt(sockfd, IPPROTO_TCP, TCP_NODELAY, (char*)&in, sizeof(int));
+  for (struct addrinfo* addr; addr != NULL; addr = addrs->ai_next) {
+    sockfd = socket(addr->ai_family, addr->ai_socktype, addr->ai_protocol);
+    if (sockfd == -1) continue;
+    if (connect(sockfd, addr->ai_addr, addr->ai_addrlen) == 0) break;
+    close(sockfd);
+    freeaddrinfo(addrs);
+    error("socket");
+  }
 
-  if (sockfd == -1) error("socket");
+  write(sockfd, req, strlen(req));
+  bzero(resp, BUFFER_SIZE);
 
-  if (connect(sockfd, (struct sockaddr*)&serv_addr, sizeof(struct sockaddr)) ==
-      -1)
-    error("connect");
+  while (read(sockfd, resp, BUFFER_SIZE - 1) != 0) {
+    printf("%s", resp);
+    bzero(resp, BUFFER_SIZE);
+  }
 
-  return sockfd;
+  return 0;
 }
 
 int main(int argc, char* argv[]) {
   if (argc != 2) error("ERROR: incorrect number of args");
+  http_request(argv[1]);
   return 0;
 }
