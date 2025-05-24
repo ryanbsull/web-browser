f4079d160c8bc6833e0c6fa98e9a96bab0ec1701
Ryan B. Sullivan <ryanbakersullivan@gmail.com>
2025-05-24 18:06:46 -0400

    web-browser: return error on detection of MacOS
    Continue development on Linux system
    
    Signed-off-by: Ryan B. Sullivan <ryanbakersullivan@gmail.com>

--- browser.c
+++ browser.c
@@ -53,6 +53,10 @@ int http_request(const char* addr) {
 }
 
 int main(int argc, char* argv[]) {
+#if defined(__APPLE__)
+  error("MacOS");
+#endif
+
   if (argc != 2) error("ERROR: incorrect number of args");
   http_request(argv[1]);
   return 0;
