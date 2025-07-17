2f34163ff9148110b0de74717de2fbd0978b0871
Ryan B. Sullivan <ryanbakersullivan@gmail.com>
2025-07-17 15:27:57 -0400

    web-browser: add in utility logging function
    Signed-off-by: Ryan B. Sullivan <ryanbakersullivan@gmail.com>

--- include/util.h
+++ include/util.h
@@ -2,5 +2,6 @@
 #define UTIL_H
 
 int error(const char* msg);
+int info(const char* msg);
 
 #endif
--- src/util.c
+++ src/util.c
@@ -5,3 +5,8 @@ int error(const char* msg) {
   perror(msg);
   exit(1);
 }
+
+int info(const char* msg) {
+  printf("INFO: %s", msg);
+  exit(1);
+}
